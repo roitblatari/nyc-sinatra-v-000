95f6844ef710a9e589a800cafb7668f4427db69e
roitblatari <roitblatari@gmail.com>
2018-02-01 15:22:41 -0500

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,17 +1,19 @@
 class FiguresController < ApplicationController
 
   get '/figures' do
-    @figures = Figures.all
+    @figures = Figure.all
     erb :'/figures/index'
   end
   
   get '/figures/new' do
+     @figures = Figure.all
     erb :'/figures/new'
   end
 
-  post '/figures/:id' do
+  post '/figures' do
     binding.pry
-    @figures = Figures.find(:id)
+    @figure = Figure.create(name: params[:figure]["name"], title: params[:figure]["title"]["name"])
+    @figure = Figure.find(params[:id])
     erb :'/figures/show'
   end
 end
